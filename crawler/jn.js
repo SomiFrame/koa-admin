d1f6e7fbe7f0496c74bebeceec93cc489183806d
Apple <frame@moonjoy.net>
2018-10-18 23:17:59 +0800

    update

--- crawler/jn.js
+++ crawler/jn.js
@@ -4,14 +4,13 @@ const cheerio = require('cheerio')
 const {M_Video} = require('../mongodb/models')
 
 const target_host = "https://javynow.com"
-const page_index = []
-const page_paly = []
 
-const start = async ()=>{
-    const result_index = await request.get(target_host).set('Accept','text/html')
+const fetchIndex = async (url)=>{
+    const result_index = await request.get(url).set('Accept','text/html')
     console.log(result_index.text)
     const $_index = cheerio.load(result_index.text)
     const links = $_index('a','figure')
+    const page_index = []
     links.map((index,a)=>{
         page_index.push({
             title: a.parent.next.next.children[0].children[0].data,
@@ -21,6 +20,19 @@ const start = async ()=>{
     })
     console.log(page_index)
     const res_insert = await M_Video.insertMany(page_index)
+    return page_index.length
+}
+
+const start = async ()=>{
+    let count =0;
+    let from = 1,
+        to = 50
+    for(;from<=to;from++) {
+        console.log(from)
+        const num = await fetchIndex(`${target_host}/?p=${from}`)
+        count = count + num
+        console.log(`已趴取:--${count}--条数据`)
+    }
 }
 
 start()
